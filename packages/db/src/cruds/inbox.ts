588afe20facc0e3886ae44606c252fd81614fdb7
yaskou <anmitutarow@gmail.com>
2026-02-01 14:17:36 +0900

    refactor: Remove unnecessary cruds

--- packages/db/src/cruds/inbox.ts
+++ packages/db/src/cruds/inbox.ts
@@ -1,4 +1,4 @@
-import { desc, eq, inArray } from "drizzle-orm";
+import { eq, inArray } from "drizzle-orm";
 import { inboxesTable } from "../schemas";
 import { InboxCreate } from "./types";
 import { Client } from "../db";
@@ -7,29 +7,10 @@ export const createInbox = async (db: Client, inbox: InboxCreate) => {
   await db.insert(inboxesTable).values(inbox);
 };
 
-export const readInboxesByUserId = async (db: Client, userId: string) => {
-  return await db.query.inboxesTable.findMany({
-    orderBy: [desc(inboxesTable.timestamp)],
-    where: eq(inboxesTable.userId, userId),
-  });
-};
-
-export const readUnreplyUserIds = async (db: Client) => {
-  return await db
-    .selectDistinct({
-      id: inboxesTable.userId,
-    })
-    .from(inboxesTable);
-};
-
 export const deleteInbox = async (db: Client, id: string) => {
   await db.delete(inboxesTable).where(eq(inboxesTable.id, id));
 };
 
 export const deleteInboxes = async (db: Client, ids: string[]) => {
   await db.delete(inboxesTable).where(inArray(inboxesTable.id, ids));
-};
-
-export const deleteInboxesByUserId = async (db: Client, userId: string) => {
-  await db.delete(inboxesTable).where(eq(inboxesTable.userId, userId));
 };
--- packages/db/src/cruds/message.ts
+++ packages/db/src/cruds/message.ts
@@ -1,4 +1,4 @@
-import { desc, eq } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { messagesTable } from "../schemas/message";
 import { MessageCreate } from "./types";
 import type { Client } from "../db";
@@ -9,14 +9,6 @@ export const createMessage = async (db: Client, messages: MessageCreate) => {
     .values(Array.isArray(messages) ? messages : [messages]);
 };
 
-export const readMessages = async (db: Client, userId: string, limit = 1) => {
-  return await db.query.messagesTable.findMany({
-    limit,
-    orderBy: [desc(messagesTable.timestamp)],
-    where: eq(messagesTable.userId, userId),
-  });
-};
-
-export const deleteMessage = async (db: Client, userId: string) => {
-  await db.delete(messagesTable).where(eq(messagesTable.id, userId));
+export const deleteMessage = async (db: Client, id: string) => {
+  await db.delete(messagesTable).where(eq(messagesTable.id, id));
 };
